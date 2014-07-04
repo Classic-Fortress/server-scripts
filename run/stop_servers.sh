3d665afdc43ffdfff08349ead2e90b7a3fdd1d6f
Niclas Lindstedt <mpezar@gmail.com>
2014-07-04 09:08:35 +0200

    Add --silent parameter

--- run/stop_servers.sh
+++ run/stop_servers.sh
@@ -1,67 +1,69 @@
 #!/bin/bash
 
+[ "$1" == "--silent" ] || silent=1
+
 # kill mvdsv
-echo -n "* Stopping mvdsv (port 27500)..."
+[ "$silent" != "1" ] || echo -n "* Stopping mvdsv (port 27500)..."
 if [ -f ~/.cfortsv/pid/server ]
 then
         pid=$(cat ~/.cfortsv/pid/server)
-        if ps -p $pid > /dev/null
+        if ps -p $pid >/dev/null
         then
                 if ps -p $pid | grep mvdsv | grep -v grep > /dev/null
                 then
-                        kill -9 $pid
-                        rm -f ~/.cfortsv/pid/server
-                        echo "[OK]"
+                        kill -9 $pid >/dev/null
+                        rm -f ~/.cfortsv/pid/server >/dev/null
+                        [ "$silent" != "1" ] || echo "[OK]"
                 else
-                        echo "[PID INCORRECT]"
+                        [ "$silent" != "1" ] || echo "[PID INCORRECT]"
                 fi
         else
-                echo "[NOT RUNNING]"
+                [ "$silent" != "1" ] || echo "[NOT RUNNING]"
         fi
 else
-        echo "[PID NOT FOUND]"
+        [ "$silent" != "1" ] || echo "[PID NOT FOUND]"
 fi
 
 # kill qtv
-echo -n "* Stopping qtv (port 28000)..."
+[ "$silent" != "1" ] || echo -n "* Stopping qtv (port 28000)..."
 if [ -f ~/.cfortsv/pid/qtv ]
 then
         pid=$(cat ~/.cfortsv/pid/qtv)
-        if ps -p $pid > /dev/null
+        if ps -p $pid >/dev/null
         then
                 if ps -p $pid | grep qtv | grep -v grep > /dev/null
                 then
-                        kill -9 $pid
-                        rm -f ~/.cfortsv/pid/qtv
-                        echo "[OK]"
+                        kill -9 $pid >/dev/null
+                        rm -f ~/.cfortsv/pid/qtv >/dev/null
+                        [ "$silent" != "1" ] || echo "[OK]"
                 else
-                        echo "[PID INCORRECT]"
+                        [ "$silent" != "1" ] || echo "[PID INCORRECT]"
                 fi
         else
-                echo "[NOT RUNNING]"
+                [ "$silent" != "1" ] || echo "[NOT RUNNING]"
         fi
 else
-        echo "[PID NOT FOUND]"
+        [ "$silent" != "1" ] || echo "[PID NOT FOUND]"
 fi
 
 # kill qwfwd
-echo -n "* Stopping qwfwd (port 30000)..."
+[ "$silent" != "1" ] || echo -n "* Stopping qwfwd (port 30000)..."
 if [ -f ~/.cfortsv/pid/qwfwd ]
 then
         pid=$(cat ~/.cfortsv/pid/qwfwd)
-        if ps -p $pid > /dev/null
+        if ps -p $pid >/dev/null
         then
                 if ps -p $pid | grep qwfwd | grep -v grep > /dev/null
                 then
-                        kill -9 $pid
-                        rm -f ~/.cfortsv/pid/qwfwd
-                        echo "[OK]"
+                        kill -9 $pid >/dev/null
+                        rm -f ~/.cfortsv/pid/qwfwd >/dev/null
+                        [ "$silent" != "1" ] || echo "[OK]"
                 else
-                        echo "[PID INCORRECT]"
+                        [ "$silent" != "1" ] || echo "[PID INCORRECT]"
                 fi
         else
-                echo "[NOT RUNNING]"
+                [ "$silent" != "1" ] || echo "[NOT RUNNING]"
         fi
 else
-        echo "[PID NOT FOUND]"
+        [ "$silent" != "1" ] || echo "[PID NOT FOUND]"
 fi
